92b3ef5b70d61dae0a44e9e805d8ef745aa35b05
BlackTecCom2000 <djaborov2000@gmail.com>
2026-08-27 01:01:35 +0500

    chore: setup github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/SUOREME-COURT-SITE-OF-TJ/',
   server: {
     port: 5173,
     host: true,
